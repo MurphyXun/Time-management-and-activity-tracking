cc03df01b8eccc3f5c3d8af362438098fae84188
Xun Sun <xunsun@10-140-46-200.wifi-m.deakin.edu.au>
2016-12-09 16:27:30 +1100

    Initial Commit

---
+++ Time management and activity tracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time management and activity tracking
+//
+//  Created by Xun Sun on 9/12/16.
+//  Copyright © 2016 Xun Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
